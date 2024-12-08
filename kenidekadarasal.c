a228fdf56e10de4e69bdc0bbff1620b4fbeb73e7
N <ktox000@gmail.com>
2024-12-08 18:59:26 +0300

    Yeni kod eklendi

---
+++ kenidekadarasal.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int num;
+    printf ("Lütfen bir sayı giriniz: \n. Çımak için #");
+    scanf ("%c", &num);
+    
+    do{
+        if (num<=0){
+        printf ("Geçersin işlem: ")
+        }
+    }
+}
